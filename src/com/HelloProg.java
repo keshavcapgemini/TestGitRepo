0507a4e8ea245ce9e891b5398f07a43e5158ae03
keshavcapgemini <keshavcapgemini@gmail.com>
2022-09-13 16:18:05 +0530

    HelloProg

---
+++ src/com/HelloProg.java
@@ -0,0 +1,19 @@
+package com;
+
+/**
+ * TODO: Document me!
+ *
+ * @author kessingh
+ *
+ */
+public class HelloProg {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
